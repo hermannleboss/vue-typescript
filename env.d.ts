9da7b7fe59eb11f61cde918b5d3dcb93ca0b7d92
Hermann EHO <hermannleboss@gmail.com>
2024-02-14 10:05:46 +0000

    Add ethereum and layer8 interfaces

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+/**
+ * Add ethereum to the window object.
+ */
+interface Window {
+    ethereum: any
+  }
+  
+  /**
+   * Add layer8 to the globalThis object.
+   */
+  interface Layer8 {
+    fetch(input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response>
+  }
+  // make globalThis.layer8 available.
+  declare const layer8: Layer8
+  
